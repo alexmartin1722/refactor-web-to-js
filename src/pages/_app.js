aac77efd767c0a485952f23d23ed9adbf6d13367
Sifeng Chen <sifengchen2002@gmail.com>
2023-12-14 13:26:51 -0500

    fixed css location

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import "@/styles/globals.css";
+import "@styles/../../styles/globals.css";
+import "@styles/../../styles/page.module.css";
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
